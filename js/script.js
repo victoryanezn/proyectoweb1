004e75ee4300eeebb2cfbd8ff0c4bffa96b23b44
Victor Yañez <163939815+victoryanezn@users.noreply.github.com>
2024-04-10 00:23:45 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('register-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Gracias por registrarte, ahora ya formas parte de webplay');
+});
+
