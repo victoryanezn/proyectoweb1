3de494a3e8f0a07b18f42be0168644095cca86c5
Victor Yañez <vi.yanezn@duocuc.cl>
2024-04-11 01:43:23 -0400

    proyecto terminado

---
+++ js/scriptnostok.js
@@ -0,0 +1,8 @@
+function toggleContent() {
+    var content = document.getElementById('content');
+    if (content.style.display === 'none') {
+        content.style.display = 'block';
+    } else {
+        content.style.display = 'none';
+    }
+}
